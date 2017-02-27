86127df0590ad0da0a20bda3b6291b4636c29876
Gonzalo Ziadi <ziadig@gmail.com>
2017-02-27 13:24:01 -0500

    Version 1.0

---
+++ content.js
@@ -0,0 +1,23 @@
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        // All anchor tags on the page
+        $('a').each(function(index, value){
+            if(value.href.indexOf("l.facebook") >= 0) {
+                if(value.href.indexOf("cnn") >= 0) {
+                    $(value).closest('._5jmm').css({"border": "5px solid red"});
+                }
+                else if(value.href.indexOf("onion") >= 0) {
+                    $(value).closest('._5jmm').css({"border": "5px solid green"});
+                }
+            }
+        })
+    })
+});
+
+var config = {
+    attributes: true,
+    childList: true,
+    characterData: true
+};
+
+observer.observe(document.body, config);
